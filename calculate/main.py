86245eb171d2c03949b5e74ac75d94a09ee42f62
Umar Suleman <usb.net@outlook.com>
2023-04-27 17:37:27 +0200

    completed/passed

--- calculate/main.py
+++ calculate/main.py
@@ -3,3 +3,17 @@ __winc_id__ = '499e67d5cb54448e93cee7465be2c866'
 __human_name__ = 'calculate'
 
 # Add your code after this line
+broccoli = 2
+leek = 2
+potato = 3
+brussel_sprout = 7
+sum_one_each = sum([broccoli, leek, potato, brussel_sprout])
+avg_price = sum_one_each/len([broccoli, leek, potato, brussel_sprout])
+num_broccolis = 5
+num_leeks = 2
+num_potatoes = 7
+num_brussel_sprouts = 10
+sum_total = sum([broccoli*num_broccolis, leek*num_leeks, potato*num_potatoes, brussel_sprout*num_brussel_sprouts])
+discount_percentage = 30
+discounted_sum_total = sum_total - (sum_total*(discount_percentage/100))
+print(discounted_sum_total)
