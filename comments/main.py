a0df14d16889a1b7770e41d3166e009981e72c98
Umar Suleman <usb.net@outlook.com>
2023-04-27 18:53:54 +0200

    Completed, Passed

---
+++ comments/main.py
@@ -0,0 +1,14 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+# code for assignment
+# assignment name is: comments
+#
+max_num_emp = 50  # maximum number of employees
+min_num_emp = 10  # minimum number of employees
+if max_num_emp > 40:
+    print("There are enough number of emplyeesa avaialable")
+""" If number of employees would be more then 40, which is actually number 50 greater than 40."""
+"""Then messege will be displayed that there are enough empoyees avaiable"""
