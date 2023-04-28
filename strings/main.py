094515e82d8337ff414892a6ebe5ba92ba0c0706
Umar Suleman <usb.net@outlook.com>
2023-04-28 22:56:44 +0200

    Update

---
+++ strings/main.py
@@ -0,0 +1,13 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player_nl_1 = "Ruud Gullit"
+player_nl_2 = "Marco van Basten"
+goal_0 = 32
+goal_1 = 54
+scorers = f"{player_nl_1} {goal_0}, {player_nl_2} {goal_1}"
+report = f"{player_nl_1} scored in the {goal_0}nd minute\n{player_nl_2} scored in the {goal_1}th minute"
+print(scorers)
+print(report)
