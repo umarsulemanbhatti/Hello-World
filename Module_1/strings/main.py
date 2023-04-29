292e2b60f43c1a0983d149a10dacd26564cf41ac
Umar Suleman <usb.net@outlook.com>
2023-04-29 21:52:56 +0200

    Completed, checked, passed

--- Module_1/strings/main.py
+++ Module_1/strings/main.py
@@ -9,5 +9,12 @@ goal_0 = 32
 goal_1 = 54
 scorers = f"{player_nl_1} {goal_0}, {player_nl_2} {goal_1}"
 report = f"{player_nl_1} scored in the {goal_0}nd minute\n{player_nl_2} scored in the {goal_1}th minute"
-print(scorers)
-print(report)
+player = "Hans van Breukelen"
+first_name = player[(player.find("Hans")):(player.find(" van"))]
+last_name = player[player.find("van"):]
+last_name_len = len(player[player.find("van"):])
+name_short = str(first_name[:1])+". "+str(last_name)
+chant = str((first_name)+"! ")*3 + str(first_name+"!")
+good_chant = chant[-1] != " "
+print(chant)
+print(good_chant)
