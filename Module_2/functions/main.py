068db0309a46cb3ccda26ef3a5b1394607c8dc04
Umar Suleman <usb.net@outlook.com>
2023-04-30 20:49:03 +0200

    functions

--- Module_2/functions/main.py
+++ Module_2/functions/main.py
@@ -3,3 +3,15 @@ __winc_id__ = '49bce82ef9cc475ca3146ee15b0259d0'
 __human_name__ = 'functions'
 
 # Add your code after this line
+def greet(name):
+    greet_name="Hello, "+name
+    return greet_name
+
+def sum(a, b, c):
+    fun_sum=a+b+c
+    return fun_sum
+
+name="Umar"
+print(greet(name))
+print(sum(2, 3, 4))
+
