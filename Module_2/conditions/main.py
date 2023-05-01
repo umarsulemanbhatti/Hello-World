339a04c393ee291e7363f1a61661e1fa0f0dcda1
Umar Suleman <usb.net@outlook.com>
2023-05-02 00:23:59 +0200

    Completed

---
+++ Module_2/conditions/main.py
@@ -0,0 +1,25 @@
+# Do not modify these lines
+__winc_id__ = '25596924dffe436da9034d43d0af6791'
+__human_name__ = 'conditions'
+
+# Add your code after this line
+def farm_action(weather, time_of_day, cow_milking_status, location_of_the_cows, season, slurry_tank, grass_status):
+    
+    if  location_of_the_cows == "pasture" and (weather == "rain" or time_of_day == "night"):
+        return "take cows to cowshed"
+    
+    elif location_of_the_cows == "cowshed" and cow_milking_status == True:
+        return "milk cows"
+    
+    elif location_of_the_cows == "cowshed" and (weather != "sunny" or weather != "windy") and slurry_tank == True:
+        return "fertilize pasture" 
+    
+    elif grass_status == True and season == "spring" and weather == "sunny" and location_of_the_cows != "pasture":
+        return "mow grass"
+    
+    elif cow_milking_status == True or (location_of_the_cows == "pasture" and (weather != "sunny" or weather != "windy") and slurry_tank == True) or grass_status==True:
+        return "take cows to cowshed\nmilk cows\ntake cows back to pasture"
+
+    else:
+        return "wait"
+print(farm_action('rain', 'night', False, 'cowshed', 'winter', False, True))
