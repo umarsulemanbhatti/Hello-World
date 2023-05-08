a9f8042677d454841262a0811bc2e0af9496e25f
Umar Suleman <usb.net@outlook.com>
2023-05-08 20:43:01 +0200

    update

---
+++ Module_2/for/main.py
@@ -0,0 +1,24 @@
+from helpers import get_countries
+
+
+""" Leave this untouched. Wincpy uses it to match this assignment with the
+tests it runs. """
+__winc_id__ = "c545bc87620d4ced81cbddb8a90b4a51"
+__human_name__ = "for"
+
+
+""" Write your functions here. """
+
+
+
+# This block is only run if this file is the entrypoint; python main.py
+# It is not run if it is imported as a module: `from main import *`
+if __name__ == "__main__":
+    countries = get_countries()
+
+    """ Write the calls to your functions here. """
+
+len_country_names = [len(country) for country in countries]
+for country in countries:
+    if len(country) == min(len_country_names):
+        print(list(country))
