fb4ae7d2e025ca176d8d3baa46c8c8443c835fb3
Umar Suleman <usb.net@outlook.com>
2023-05-09 22:16:49 +0200

    update

--- Module_2/for/helpers.py
+++ Module_2/for/helpers.py
@@ -6,3 +6,4 @@ def get_countries():
     module_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     data_fp = open(os.path.join(module_path, "countries.json"), "r")
     return json.load(data_fp)["countries"]
+    
--- Module_2/for/main.py
+++ Module_2/for/main.py
@@ -10,16 +10,12 @@ __human_name__ = "for"
 """ Write your functions here. """
 
 
-
 # This block is only run if this file is the entrypoint; python main.py
 # It is not run if it is imported as a module: `from main import *`
 if __name__ == "__main__":
     countries = get_countries()
+   
+    
 
     """ Write the calls to your functions here. """
-
-len_country_names = [len(country) for country in countries]
-for country in countries:
-    if len(country) == min(len_country_names):
-        print(country)
 
